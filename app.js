c5f3604770b9e00b1a4f97556ca9e4617dace83c
Ryan James Dorsey <epiego@gmail.com>
2021-02-12 14:50:41 -0800

    made function

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // import functions and grab DOM elements
+import { sync } from '../dom-utilities.js';
 
-const noun1 = document.getElementById  
+const noun1 = document.getElementById(noun-1);
+const noun2 = document.getElementById(noun-2);
+const noun3 = document.getElementById(noun-3);
+const noun4 = document.getElementById(noun-4);
+
+const verb1 = document.getElementById(verb-1);
+const verb2 = document.getElementById(verb-2);
+const verb3 = document.getElementById(verb-3);
+
+const adjective1 = document.getElementById(adj-1);
+const adjective2 = document.getElementById(adj-2);
+
+createButton.addEventListener('click', () => {
+    const answerNoun1 = sync(noun1.textContent, );  
+}
+
+
+
+
 
 // initialize state
 
