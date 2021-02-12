13030b6ce322f0df6d3e07ab44b8f9181cbaf073
Ryan James Dorsey <epiego@gmail.com>
2021-02-12 13:36:08 -0800

    initial setup and paragraph

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
 
-const test = QUnit.test;
-
-test('time to test a function', (expect) => {
+import { dom-utilities.js } from '../dom-utilities.js';
+test('sync input to element', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const expected = 'banana';
+    input.value = expected;
     
     //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+    sync(input, span);
 
+
+    export
     //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(span.textContent, expected);
 });
+
